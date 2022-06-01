3ae80f5470ec211bf7ac8218144067b8e8929f38
Sergey Turinskiy <turinskiy@gmail.com>
2022-06-01 11:54:58 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import "./styles.css";
+
+function useGiphy(query) {
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setLoading(true);
+        const response = await fetch(
+          `https://api.giphy.com/v1/gifs/search?api_key=vNiWFDmaWvqLfp0zT7w7TP87jrieGvGv&q=${query}&limit=25&offset=0&rating=g&lang=en`
+        );
+        const json = await response.json();
+        console.log({ json });
+        setResults(
+          json.data.map((item) => {
+            return item.images.preview.mp4;
+          })
+        );
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    if (query !== "") {
+      fetchData();
+    }
+  }, [query]);
+
+  return [results, loading];
+}
+export default function App() {
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("");
+  const [results, loading] = useGiphy(query);
+
+  function onSubmit(e) {
+    e.preventDefault();
+    setQuery(search);
+  }
+
+  // vNiWFDmaWvqLfp0zT7w7TP87jrieGvGv
+  return (
+    <div className="App">
+      <form onSubmit={onSubmit}>
+        <input
+          value={search}
+          placeholder="Search for GIPHY"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
+      <br />
+      {loading ? (
+        <h3>Loading...</h3>
+      ) : (
+        results.map((item) => <video autoPlay loop key={item} src={item} />)
+      )}
+    </div>
+  );
+}
